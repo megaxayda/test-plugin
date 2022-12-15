b60f18b7cdaad44e9fe29df784c7f751fc46ca26
Tai Pham <tai@newlogic.com>
2022-12-15 16:44:54 +0700

    add capacitor

---
+++ android/app/src/main/java/com/tai/testplugin/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tai.testplugin;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tai.testplugin',
+  appName: 'vite-reactts-eslint-prettier',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
